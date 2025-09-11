5db21db9b00db8a91c039103525d47dd965aee37
Vagelis Kosmiadis <vageliskosmiadis@gmail.com>
2025-09-11 12:00:27 +0300

    Added hero content to home page

--- src/components/layouts/CoreLayout.tsx
+++ src/components/layouts/CoreLayout.tsx
@@ -2,9 +2,9 @@ import Header from "@components/layout/Header";
 import { Outlet } from "react-router-dom";
 
 export default function CoreLayout () {
-    return <div className="w-full max-w-(--max-width) mx-auto p-4">
+    return <div className="w-full flex flex-col gap-8 min-h-screen max-w-(--max-width) mx-auto p-4 text-stone-800">
         <Header />
-        <main>
+        <main className="flex w-full">
             <Outlet />
         </main>
     </div>
--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -6,6 +6,6 @@ interface ButtonI extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 
 export default function Button ({ children }: ButtonI) {    
     return <button
-    className={`hover:cursor-pointer hover:bg-accent/90 rounded-lg px-4 py-1 duration-175 bg-accent shadow-md text-white text-lg`}
+    className={`hover:cursor-pointer hover:bg-accent/90 text-nowrap rounded-lg px-4 py-1 duration-175 bg-accent shadow-md text-white text-lg`}
     >{children}</button>
 }
