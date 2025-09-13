01b60bcbaa95c59da5798f39d23f4dd1800bda51
Vagelis Kosmiadis <vageliskosmiadis@gmail.com>
2025-09-13 12:17:33 +0300

    Created button ui component

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -4,8 +4,9 @@ interface ButtonI extends React.ButtonHTMLAttributes<HTMLButtonElement> {
     children: ReactNode      
 }   
 
-export default function Button ({ children }: ButtonI) {    
+export default function Button ({ children, ...props }: ButtonI) {    
     return <button
-    className={`hover:cursor-pointer hover:bg-accent/90 text-nowrap rounded-lg px-4 py-1 duration-175 bg-accent shadow-md text-white text-lg`}
+    className={`hover:cursor-pointer hover:bg-accent/90 text-nowrap rounded-lg px-4 py-1 duration-175 shadow-md text-white text-lg`}
+    {...props}
     >{children}</button>
 }
