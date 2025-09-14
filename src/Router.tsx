fe8c373af25b18abb6149d80f0f6237d6c4fb397
Vagelis Kosmiadis <vageliskosmiadis@gmail.com>
2025-09-14 20:38:49 +0300

    Created Auth Layout and Pages

--- src/Router.tsx
+++ src/Router.tsx
@@ -7,6 +7,9 @@ import Links from "@components/pages/Dashboard/Links";
 import Analytics from "@components/pages/Dashboard/Analytics";
 import Settings from "@components/pages/Dashboard/Settings";
 import Subscription from "@components/pages/Dashboard/Subscription";
+import AuthLayout from "@components/layouts/AuthLayout";
+import Login from "@components/pages/Auth/Login";
+import Signup from "@components/pages/Auth/Signup";
 
 export const Router = createBrowserRouter([
     { path: '/', element: <CoreLayout />, children: [
@@ -17,6 +20,11 @@ export const Router = createBrowserRouter([
             { path: 'analytics', element: <Analytics /> },
             { path: 'subscription', element: <Subscription />},
             { path: 'settings', element: <Settings />},
+
+        ]},
+        { path: 'auth', element: <AuthLayout />, children: [
+            {path: 'login', element: <Login />},
+            {path: 'signup', element: <Signup />},
 
         ]}
     ]}
