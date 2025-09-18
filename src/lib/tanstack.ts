e64f3a2b4c8962f1ba8621674d2daa31a2fc1ff9
Vagelis Kosmiadis <vageliskosmiadis@gmail.com>
2025-09-18 11:03:16 +0300

    Added queryConfig for queries

--- src/lib/tanstack.ts
+++ src/lib/tanstack.ts
@@ -1,4 +1,13 @@
-import { QueryClient } from "@tanstack/react-query";
+import { QueryClient, type DefaultOptions } from "@tanstack/react-query";
+
+
+export const queryConfig = {
+    queries: {
+        refetchOnWindowFocus: false,
+        retry: false,
+        staleTime: 1000 * 60
+    }
+} satisfies DefaultOptions
 
 export const queryClient = new QueryClient({
     defaultOptions: { queries: { retry: 0 }, mutations: { retry: 0 }}
