72813888e5fe0c2f39a0db57432dd06676ada240
Vagelis Kosmiadis <vageliskosmiadis@gmail.com>
2025-09-18 10:45:00 +0300

    Updated response interceptor with auth error handling

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,5 +1,6 @@
 import axios, { type InternalAxiosRequestConfig } from "axios";
 import { AppConfig } from "@config/AppConfig";
+import { paths } from "@config/paths";
 
 export const API = axios.create({
     baseURL: AppConfig.baseUrl,
@@ -18,8 +19,18 @@ function authRequestInterceptor (config: InternalAxiosRequestConfig) {
 
 API.interceptors.request.use(authRequestInterceptor);
 API.interceptors.response.use((response) => response.data, (error) => {
-    const errorMsg = error.response?.data?.message || error.message;
+    
     //display error notification with the errorMsg
+    const errorMsg = error.response?.data?.message || error.message;
+    //add logic here
+
+    if (error.response?.status === 401) {
+        const searchParams = new URLSearchParams();
+        const redirectTo = searchParams.get('redirectTo') || window.location.pathname;
+        window.location.href = paths.auth.login.getHref(redirectTo);
+    }
+
+    return Promise.reject(error);
 })
 
 
