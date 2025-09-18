d4a9ae857d6a6fdcd6cbe3f0ea7cdf6ab4d81f9f
Vagelis Kosmiadis <vageliskosmiadis@gmail.com>
2025-09-18 10:35:54 +0300

    Added request and response interceptors to API instance

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,9 +1,25 @@
-import axios from "axios";
+import axios, { type InternalAxiosRequestConfig } from "axios";
 import { AppConfig } from "@config/AppConfig";
 
 export const API = axios.create({
     baseURL: AppConfig.baseUrl,
     withCredentials: true,
+})
+
+
+function authRequestInterceptor (config: InternalAxiosRequestConfig) {
+    if (config.headers) {
+        config.headers.Accept = 'application/json';
+    }
+
+    config.withCredentials = true;
+    return config
+}
+
+API.interceptors.request.use(authRequestInterceptor);
+API.interceptors.response.use((response) => response.data, (error) => {
+    const errorMsg = error.response?.data?.message || error.message;
+    //display error notification with the errorMsg
 })
 
 
