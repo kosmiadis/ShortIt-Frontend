90877b7d77c1b4bae93552f0313e645d098e7659
Vagelis Kosmiadis <vageliskosmiadis@gmail.com>
2025-09-23 17:10:25 +0300

    Finished Login data validation

--- src/components/pages/Auth/Login.tsx
+++ src/components/pages/Auth/Login.tsx
@@ -1,15 +1,46 @@
 import Button from "@components/ui/Button";
 import Input from "@components/ui/Input";
 import NavigationLink from "@components/ui/NavigationLink";
+import { LoginInputSchema } from "@lib/auth/login";
+import { useRef } from "react";
+import { useLogin } from "@mutations/auth/useLogin";
+import { getZodErrorMessages } from "@util/getZodErrorMessages";
+import type { ZodIssue } from "zod/v3";
+import { toast } from "sonner";
 
 export default function Login () {
+    
+    const emailInputRef = useRef<HTMLInputElement>(null);
+    const passwordInputRef = useRef<HTMLInputElement>(null);
+
+    const { mutate: login, isPending } = useLogin();
+    
+    function handleLogin (e: React.FormEvent) {
+        e.preventDefault();
+        const email = emailInputRef.current?.value;
+        const password = passwordInputRef.current?.value;
+
+        if (!email || !password) toast.error('Please fill in all empty fields')
+
+        const isValid = LoginInputSchema.safeParse({ email, password })
+        
+        if (!isValid.success) {
+            const errorMessages = getZodErrorMessages(isValid.error.issues as ZodIssue[]);
+            errorMessages.forEach(errMsg => toast.error(errMsg))
+            console.log(errorMessages);
+            return;
+        }
+        //data validation passed
+        login({ email: email!, password: password! })
+    }
+
     return <>
         
         <h1 className="text-2xl font-semibold">Login</h1>
-        <form className=" flex flex-col gap-sm">
-            <Input className="auth-input" placeholder="Email" type="email" />
-            <Input className="auth-input" placeholder="Password" type="password"/>
-            <Button important >Login</Button>
+        <form onSubmit={handleLogin} className=" flex flex-col gap-sm">
+            <Input ref={emailInputRef} className="auth-input" placeholder="Email" type="text" />
+            <Input ref={passwordInputRef} className="auth-input" placeholder="Password" type="password"/>
+            <Button type="submit" important disabled={isPending}>Login</Button>
         </form>
 
         <h2>Don't have an account? <NavigationLink to={'/auth/signup'}>Create one</NavigationLink></h2>
