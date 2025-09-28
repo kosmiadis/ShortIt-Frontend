45c9bedd913715bb670e56af4cedfcda321ec7a9
kosmiadis <vageliskosmiadis@gmail.com>
2025-09-28 22:56:07 +0300

    Added redirect logic after successful login

--- src/mutations/auth/useLogin.tsx
+++ src/mutations/auth/useLogin.tsx
@@ -1,11 +1,22 @@
 import { login } from "@lib/auth/login";
 import { useMutation } from "@tanstack/react-query";
 
+import { useLocation, useNavigate } from "react-router-dom";
+
 export function useLogin () {
+
+    const location = useLocation();
+    const navigate = useNavigate();
+
     return useMutation({
         mutationFn: ({
             email, 
             password
-        }: { email: string, password: string }) => login({ email, password })
+        }: { email: string, password: string }) => login({ email, password }),
+        onSuccess: () => {
+            //redirect to "redirectTo" parameter
+            const redirectTo = decodeURIComponent(location.search.split('=')[1])
+            navigate(redirectTo);
+        }
     })
 }
